af10c5435d1e9622c9aee2203c3fd6f9cebbaa55
Abel Romer <abel.romer@questu.ca>
2018-03-01 15:06:59 -0800

    started the building the basic structure for a pref data entry page. added file select.views.js for jquery code for selecting which pref view.

---
+++ static/select_view.js
@@ -0,0 +1,12 @@
+$("#select-employer-prefs").on("click", function() {
+    show_view("#employer-prefs-shell", build_index_bar());
+})
+
+$("#select-employee-prefs").on("click", function() {
+    show_view("#employee-prefs-shell", build_index_bar());
+})
+
+$("#select-view-schedule").on("click", function() {
+    show_view("#view-schedule-shell", build_index_bar());
+})
+
