80a442cf3937a046a322cdc01befd55620fef0fd
Abel Romer <abel.romer@questu.ca>
2018-03-29 18:05:13 -0700

    fixed a small bug with the roles dropdown

--- static/schedule_manager/shift_setup.js
+++ static/schedule_manager/shift_setup.js
@@ -16,9 +16,9 @@ $(function () {
 $(function () {
     schedule_id = $("#shift-setup-tab").data("schedule-id")
     schedule_dates = $("#shift-setup-tab").data("schedule-dates").split(" ")
+    $.getJSON("/_api/get_roles", getRoles)
     //the code below runs, although pycharm interprets it as being commented out
     $.getJSON("/api/get_shift_data/" + schedule_dates[0].replace(/\//g, "") + "/" + schedule_id, renderShiftTable)
-    $.getJSON("/_api/get_roles", getRoles)
 })
 
 function getRoles(data) {
